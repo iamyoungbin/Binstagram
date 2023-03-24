78a543e053c11e1dfedd97c6f6f22d8dc0b508d0
iamyoungbin <binkorea2@gmail.com>
2023-03-24 18:53:58 +0900

    피드 DB와 html 연동

--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')  # == select * from Feed; (sql)
+
+        return render(request, "binstagram/main.html", context=dict(feeds=feed_list))
