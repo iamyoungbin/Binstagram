489f18dc8f54bd868da3f878b49b81586972368e
iamyoungbin <binkorea2@gmail.com>
2023-04-03 21:11:21 +0900

    피드 업로드 API 구현

--- content/views.py
+++ content/views.py
@@ -1,5 +1,11 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from Binstragram.settings import MEDIA_ROOT
 from .models import Feed
 
 # Create your views here.
@@ -8,3 +14,23 @@ class Main(APIView):
         feed_list = Feed.objects.all().order_by('-id')  # == select * from Feed; (sql)
 
         return render(request, "binstagram/main.html", context=dict(feeds=feed_list))
+
+class UploadFeed(APIView):
+    def post(self, request):
+        file = request.FILES['file']
+        # 영어와 숫자로 이루어진 고유한 아이디(이름)을 주기 위해
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = uuid_name
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+
+        return Response(status=200)
