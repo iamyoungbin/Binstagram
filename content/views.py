92772a3ba2a0b8863e08034e9af8496c3b85ad7e
iamyoungbin <binkorea2@gmail.com>
2023-04-18 16:30:58 +0900

    피드 댓글 작성 API 구현

--- content/views.py
+++ content/views.py
@@ -6,12 +6,30 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from user.models import User
 from Binstragram.settings import MEDIA_ROOT
-from .models import Feed
+from .models import Feed, Reply, Like, Bookmark
+
 
 # Create your views here.
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id')  # == select * from Feed; (sql)
+        feed_object_list = Feed.objects.all().order_by('-id')  # == select * from Feed; (sql)
+        feed_list = []
+
+        for feed in feed_object_list:
+            user = User.objects.filter(email=feed.email).first()
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=user.nickname))
+            feed_list.append(dict(id=feed.id,
+                                  image=feed.image,
+                                  content=feed.content,
+                                  like_count=feed.like_count,
+                                  profile_image=user.profile_image,
+                                  nickname=user.nickname,
+                                  reply_list=reply_list))
 
         email = request.session.get('email', None)
         # 로그인 없이 메인에 접근하는 경우
@@ -26,6 +44,7 @@ class Main(APIView):
 
         return render(request, "binstagram/main.html", context=dict(feeds=feed_list, user=user))
 
+
 class UploadFeed(APIView):
     def post(self, request):
         file = request.FILES['file']
@@ -39,12 +58,12 @@ class UploadFeed(APIView):
 
         image = uuid_name
         content = request.data.get('content')
-        user_id = request.data.get('user_id')
-        profile_image = request.data.get('profile_image')
+        email = request.session.get('email', None)
 
-        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+        Feed.objects.create(image=image, content=content, email=email, like_count=0)
 
         return Response(status=200)
+
 
 class Profile(APIView):
     def get(self, request):
@@ -61,3 +80,15 @@ class Profile(APIView):
 
         return render(request, "content/profile.html", context=dict(user=user))
 
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id,
+                             reply_content=reply_content,
+                             email=email, )
+
+        return Response(status=200)
